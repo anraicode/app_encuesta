7b632131a517d059b45fd6d207a7b1145a9fb738
anraii <enriquenun95@gmail.com>
2017-10-06 02:02:20 -0500

    v0.1

--- app_encuesta/settings.py
+++ app_encuesta/settings.py
@@ -121,11 +121,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 #STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'media'),
+    #os.path.join(BASE_DIR, 'media'),
     os.path.join(BASE_DIR, 'static'),
 )
 
